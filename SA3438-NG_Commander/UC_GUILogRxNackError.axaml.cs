6343702ebfa7b631c8c9c2c6a9c29c398bb2ad28
Johan Leroy <johan.leroy@openehs.co.uk>
2023-03-03 09:45:51 +0000

    *Add User Control for RX NACK ERROR messages

---
+++ SA3438-NG_Commander/UC_GUILogRxNackError.axaml.cs
@@ -0,0 +1,18 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace NG_Commander;
+
+public partial class UC_GUILogRxNackError : UserControl
+{
+    public UC_GUILogRxNackError()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
